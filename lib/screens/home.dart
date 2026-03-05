06abce3c8dc78630f0f1fdd21ef5a79288eb65fd
itxmal03 <itxmal03@users.noreply.github.com>
2026-03-05 12:55:49 +0500

    Bottom navigation bar added

---
+++ lib/screens/home.dart
@@ -0,0 +1,218 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:globe_trek/core/constants/size_config.dart';
+import 'package:globe_trek/screens/authScreens/sign_in_screen.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List horipics = [
+    {'img': 'assets/images/hori1.jpeg', 'name': 'Bali Highlands'},
+    {'img': 'assets/images/hori2.jpeg', 'name': 'Maldives'},
+    {'img': 'assets/images/hori3.jpeg', 'name': 'Banff, Canada'},
+    {'img': 'assets/images/hori4.jpg', 'name': 'Swiss Alps'},
+  ];
+
+  List vertipics = [
+    {
+      'img': 'assets/images/verti1.jpg',
+      'name': 'Murree',
+      'subtitle': 'Mist in the Pines',
+    },
+    {
+      'img': 'assets/images/verti2.jpg',
+      'name': 'Nathia Gali',
+      'subtitle': 'Nature’s Whispering Trails',
+    },
+    {
+      'img': 'assets/images/verti3.jpeg',
+      'name': 'Swat Valley',
+      'subtitle': 'Mini Switzerland of Pakistan',
+    },
+    {
+      'img': 'assets/images/verti5.jpg',
+      'name': 'Hunza',
+      'subtitle': 'Where Peaks Touch the Sky',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              SizedBox(height: 20),
+              Container(
+                height: SizeConfig.hp(25),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  image: DecorationImage(
+                    image: AssetImage("assets/image.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    gradient: LinearGradient(
+                      colors: [Colors.black54, Colors.transparent],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      "Explore the World",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Popular',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (_) => const SignInScreen()),
+                      );
+                    },
+                    child: Text(
+                      'See All',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+
+              SizedBox(
+                height: 240,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  itemCount: horipics.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 165,
+                      margin: EdgeInsets.symmetric(horizontal: 6),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(11),
+                        image: DecorationImage(
+                          image: AssetImage(horipics[index]['img']),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      alignment: Alignment.bottomLeft,
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        horipics[index]['name'],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor: Colors.black45,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Nearest Places',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    'See All',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ListView.builder(
+                scrollDirection: Axis.vertical,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: vertipics.length,
+                shrinkWrap: true,
+
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: ListTile(
+                      title: Text(
+                        vertipics[index]['name'],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      subtitle: Text(vertipics[index]['subtitle']),
+                      trailing: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(11),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Explore',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      leading: ClipRRect(
+                        borderRadius: BorderRadiusGeometry.circular(11),
+                        child: Image.asset(
+                          vertipics[index]['img'],
+                          fit: BoxFit.cover,
+                          width: 100,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:globe_trek/screens/authScreens/sign_in_screen.dart';
+import 'package:globe_trek/core/constants/size_config.dart';
+import 'package:globe_trek/screens/favourites_screen.dart';
+import 'package:globe_trek/screens/home.dart';
+import 'package:globe_trek/screens/profile_screen.dart';
+import 'package:globe_trek/screens/search_screen.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -10,208 +13,46 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  List horipics = [
-    {'img': 'assets/images/hori1.jpeg', 'name': 'Bali Highlands'},
-    {'img': 'assets/images/hori2.jpeg', 'name': 'Maldives'},
-    {'img': 'assets/images/hori3.jpeg', 'name': 'Banff, Canada'},
-    {'img': 'assets/images/hori4.jpg', 'name': 'Swiss Alps'},
+  final List<Widget> _screens = [
+    Home(),
+    SearchScreen(),
+    FavouritesScreen(),
+    ProfileScreen(),
   ];
 
-  List vertipics = [
-    {
-      'img': 'assets/images/verti1.jpg',
-      'name': 'Murree',
-      'subtitle': 'Mist in the Pines',
-    },
-    {
-      'img': 'assets/images/verti2.jpg',
-      'name': 'Nathia Gali',
-      'subtitle': 'Nature’s Whispering Trails',
-    },
-    {
-      'img': 'assets/images/verti3.jpeg',
-      'name': 'Swat Valley',
-      'subtitle': 'Mini Switzerland of Pakistan',
-    },
-    {
-      'img': 'assets/images/verti5.jpg',
-      'name': 'Hunza',
-      'subtitle': 'Where Peaks Touch the Sky',
-    },
-  ];
+  int _selectedIndex = 0;
+
+  void selectedScreen(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig.init(context);
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                SizedBox(height: 40),
-                SizedBox(
-                  width: 350,
-                  child: Text(
-                    'Find your Happiness with Us!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                SizedBox(
-                  height: 45,
-                  width: 350,
-                  child: SizedBox(
-                    width: 300,
-                    child: Center(
-                      child: SearchBar(
-                        hintText: 'search your destination',
-                        trailing: [Icon(Icons.search)],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Popular',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () async {
-                        await FirebaseAuth.instance.signOut();
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => const SignInScreen(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'See All',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-
-                SizedBox(
-                  height: 240,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    physics: BouncingScrollPhysics(),
-                    itemCount: horipics.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 165,
-                        margin: EdgeInsets.symmetric(horizontal: 6),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(11),
-                          image: DecorationImage(
-                            image: AssetImage(horipics[index]['img']),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        alignment: Alignment.bottomLeft,
-                        padding: EdgeInsets.all(8),
-                        child: Text(
-                          horipics[index]['name'],
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                            backgroundColor: Colors.black45,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Nearest Places',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    Text(
-                      'See All',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: vertipics.length,
-                  shrinkWrap: true,
-
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: ListTile(
-                        title: Text(
-                          vertipics[index]['name'],
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        subtitle: Text(vertipics[index]['subtitle']),
-                        trailing: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadiusGeometry.circular(11),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'Explore',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadiusGeometry.circular(11),
-                          child: Image.asset(
-                            vertipics[index]['img'],
-                            fit: BoxFit.cover,
-                            width: 100,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Explore'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
           ),
-        ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        useLegacyColorScheme: true,
+        backgroundColor: Colors.white,
+        elevation: 2,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 28,
       ),
     );
   }
